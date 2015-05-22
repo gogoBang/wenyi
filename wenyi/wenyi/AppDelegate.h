e8ac0546df51de42d037b2742d6da9ff83541d19
gogoBang <alex@172.20.1.72>
2015-05-22 19:44:55 +0800

    项目添加
    create the new project

---
+++ wenyi/wenyi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wenyi
+//
+//  Created by Alex Wu on 15/5/22.
+//  Copyright (c) 2015年 Alex Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wenyi/wenyi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wenyi
+//
+//  Created by Alex Wu on 15/5/22.
+//  Copyright (c) 2015年 Alex Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
